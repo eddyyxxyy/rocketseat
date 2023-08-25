2089b011c44cf3ed25180c2108cab3a08418f6a2
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-08-25 18:12:15 -0300

    feat: adiciona método `isOpen` no `Modal` | Adiciona funcionalidade de fechar o `Modal` caso aperte a tecla ESC

--- Explorer/Stage-05/calculo-de-imc/solucao/js/modal.js
+++ Explorer/Stage-05/calculo-de-imc/solucao/js/modal.js
@@ -2,9 +2,20 @@ export const Modal = {
   toggle() {
     this.wrapper.classList.toggle("open");
   },
+  isOpen() {
+    return this.wrapper.classList.contains("open");
+  },
   wrapper: document.querySelector(".modal-wrapper"),
   message: document.querySelector(".modal .title span"),
   btnClose: document.querySelector(".modal button.close"),
 };
 
 Modal.btnClose.onclick = () => Modal.toggle();
+
+window.addEventListener("keydown", handleKeyDown);
+
+function handleKeyDown(event) {
+  if (event.key === "Escape" && Modal.isOpen()) {
+    Modal.toggle();
+  }
+}
