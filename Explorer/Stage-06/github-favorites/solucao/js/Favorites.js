ad9d329655f61a370d8856b84b59e68840c7acf5
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-09-04 20:36:46 -0300

    feat: finaliza a solução do projeto Github Favorites

---
+++ Explorer/Stage-06/github-favorites/solucao/js/Favorites.js
@@ -0,0 +1,118 @@
+import { GithubUser } from "./GithubUser.js";
+
+class Favorites {
+  constructor(root) {
+    this.root = document.querySelector(root);
+    this.load();
+  }
+
+  delete(user) {
+    const filteredEntries = this.entries.filter(
+      (entry) => entry.login !== user.login
+    );
+    this.entries = filteredEntries;
+    this.update();
+    this.save();
+  }
+
+  load() {
+    this.entries = JSON.parse(localStorage.getItem("@github-favorites:")) || [];
+  }
+}
+
+export class FavoritesView extends Favorites {
+  constructor(root) {
+    super(root);
+
+    this.tbody = this.root.querySelector("table tbody");
+
+    this.update();
+    this.onAdd();
+  }
+
+  async add(userName) {
+    try {
+      const userExists = this.entries.find((entry) => entry.login === userName);
+      if (userExists) {
+        throw new Error("Usuário já cadastrado!");
+      }
+
+      const user = await GithubUser.search(userName);
+      if (user.login === undefined) {
+        throw new Error("Usuário não encontrado!");
+      }
+      this.entries = [user, ...this.entries];
+      this.save();
+      this.update();
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  createRow() {
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
+      <td class="user">
+        <img
+          src=""
+          alt=""
+        />
+        <a href="" target="_blank">
+          <p></p>
+          <span></span>
+        </a>
+      </td>
+      <td class="repositories"></td>
+      <td class="followers"></td>
+      <td><button type="button" class="remove">&times;</button></td>
+    `;
+
+    return tr;
+  }
+
+  onAdd() {
+    const addButton = this.root.querySelector(".search button");
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector(".search input");
+      this.add(value);
+    };
+  }
+
+  removeAllTr() {
+    this.tbody.querySelectorAll("tr").forEach((tr) => {
+      tr.remove();
+    });
+  }
+
+  save() {
+    localStorage.setItem("@github-favorites:", JSON.stringify(this.entries));
+  }
+
+  update() {
+    this.removeAllTr();
+    this.entries.forEach((user) => {
+      const row = this.createRow();
+
+      row.querySelector(
+        ".user img"
+      ).src = `https://github.com/${user.login}.png`;
+      row.querySelector(".user img").alt = `Imagem de ${user.name}`;
+      row.querySelector(".user a").href = `https://github.com/${user.login}`;
+      row.querySelector(".user p").textContent = user.name;
+      row.querySelector(".user span").textContent = user.login;
+      row.querySelector(".repositories").textContent = user.public_repos;
+      row.querySelector(".followers").textContent = user.followers;
+
+      row.querySelector(".remove").onclick = () => {
+        const isOk = confirm("Tem certeza que deseja deletar essa linha?");
+
+        if (isOk) {
+          this.delete(user);
+        }
+      };
+
+      this.tbody.append(row);
+    });
+  }
+}
