594b22aead7acf7851e6a62ea607a890e06f297d
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-09-07 15:20:13 -0300

    feat: implementa update do usuário

--- Explorer/Stage-08/first-api/RocketNotes/src/controllers/UsersController.js
+++ Explorer/Stage-08/first-api/RocketNotes/src/controllers/UsersController.js
@@ -1,6 +1,6 @@
 const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
-const { hash } = require("bcrypt");
+const { compare, hash } = require("bcrypt");
 
 class UsersController {
   async create(req, resp) {
@@ -24,6 +24,53 @@ class UsersController {
     );
 
     return resp.status(201).json();
+  }
+
+  async update(req, resp) {
+    const { name, email, password, old_password } = req.body;
+    const { id } = req.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+    if (!user) {
+      throw new AppError("Usuário não encontrado");
+    }
+
+    const userWithUpdatedEmail = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso.");
+    }
+
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if (password && !old_password) {
+      throw new AppError(
+        "Você precisar informar a senha antiga para poder definir uma nova."
+      );
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if (!checkOldPassword) {
+        throw new AppError("A senha antiga não está correta.");
+      }
+
+      user.password = await hash(password, 8);
+    }
+
+    await database.run(
+      `UPDATE users SET name = (?), email = (?), password = (?), modified_at = DATETIME('now') WHERE id = (?)`,
+      [user.name, user.email, user.password, id]
+    );
+
+    return resp.json();
   }
 }
 
