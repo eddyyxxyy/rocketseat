a0e9b4f9810e9e454ac6bf372faa74c18a66220b
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-09-12 10:45:36 -0300

    chore: adds documentation to classes

--- Explorer/Stage-08/RocketMovies/src/controllers/UsersController.js
+++ Explorer/Stage-08/RocketMovies/src/controllers/UsersController.js
@@ -1,8 +1,23 @@
 const knex = require("../database/knex");
 
+/**
+ * UsersController class
+ * @class
+ */
 class UsersController {
+  /**
+   * Creates a new user.
+   *
+   * This is an asynchronous function that receives
+   * a request and a response object, and handles the
+   * data that will be registered in the Database.
+   *
+   * @param {Object} req - The request object
+   * @param {Object} res - The response object
+   * @returns {Object} JSON response with a message/data
+   */
   async create(req, res) {
-    res.json({ message: "Testing UsersController /create route." });
+    return res.json({ message: "Testing UsersController /create route." });
   }
 }
 
--- Explorer/Stage-08/RocketMovies/src/utils/AppError.js
+++ Explorer/Stage-08/RocketMovies/src/utils/AppError.js
@@ -1,7 +1,23 @@
+/**
+ * AppError class
+ * @class
+ */
 class AppError {
+  /**
+   * @property {string} message - The error message
+   */
   message;
+  /**
+   * @property {number} statusCode - The HTTP status code, defaults to 400
+   */
   statusCode;
 
+  /**
+   * Create an AppError
+   * @constructor
+   * @param {string} message - The error message
+   * @param {number} [statusCode=400] - The HTTP status code
+   */
   constructor(message, statusCode = 400) {
     this.message = message;
     this.statusCode = statusCode;
