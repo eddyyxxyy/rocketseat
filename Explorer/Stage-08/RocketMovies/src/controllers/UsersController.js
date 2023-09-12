49d2f5e658002cdab1386107fde166627d265e9d
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-09-12 13:41:45 -0300

    feat: implements User creation logic, resource is now live

--- Explorer/Stage-08/RocketMovies/src/controllers/UsersController.js
+++ Explorer/Stage-08/RocketMovies/src/controllers/UsersController.js
@@ -1,4 +1,8 @@
 const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const userExists = require("../utils/userExists");
+const checkUserRequestData = require("../utils/checkUserRequestData");
+const { hash, compare } = require("bcrypt");
 
 /**
  * UsersController class
@@ -12,12 +16,28 @@ class UsersController {
    * a request and a response object, and handles the
    * data that will be registered in the Database.
    *
-   * @param {Object} req - The request object
-   * @param {Object} res - The response object
-   * @returns {Object} JSON response with a message/data
+   * @param {object} req - The request object
+   * @param {object} res - The response object
+   * @returns {null} JSON response with a message/data to the client
    */
   async create(req, res) {
-    return res.json({ message: "Testing UsersController /create route." });
+    const { name, email, password } = req.body;
+
+    if (await userExists(knex, email)) {
+      throw new AppError("E-mail already in use, try another.");
+    }
+
+    const errors = checkUserRequestData({ name, email, password });
+
+    if (Object.keys(errors).length > 0) {
+      throw new AppError(errors);
+    }
+
+    const encryptedPassword = await hash(password, 8);
+
+    await knex("users").insert({ name, email, password: encryptedPassword });
+
+    res.status(201).json({ message: `User '${name}' created with success` });
   }
 }
 
