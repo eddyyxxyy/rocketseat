97b65f2b51174411f0a5673f1cfcf29324c37c4c
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-09-14 11:23:17 -0300

    feat: implementa página de SignUp

--- Explorer/Stage-09/rocketnotes/src/pages/SignUp/index.jsx
+++ Explorer/Stage-09/rocketnotes/src/pages/SignUp/index.jsx
@@ -1,21 +1,22 @@
 import { BackgroundImg, Container, Form } from './style';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
-import { FiMail, FiLock } from 'react-icons/fi';
+import { FiUser, FiMail, FiLock } from 'react-icons/fi';
 
-export function SignIn() {
+export function SignUp() {
   return (
     <Container>
+      <BackgroundImg />
       <Form>
         <h1>Rocket Notes</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis.</p>
-        <h2>Faça seu login</h2>
+        <h2>Crie sua conta</h2>
+        <Input type="text" icon={FiUser} placeholder="Nome" />
         <Input type="email" icon={FiMail} placeholder="E-mail" />
         <Input type="password" icon={FiLock} placeholder="Senha" />
-        <Button text="Entrar" />
-        <a href="#">Criar conta</a>
+        <Button text="Cadastrar" />
+        <a href="#">Voltar para o login</a>
       </Form>
-      <BackgroundImg />
     </Container>
   );
 }
