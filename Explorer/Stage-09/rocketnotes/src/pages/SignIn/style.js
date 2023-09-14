3b048b22957cacac6c723a29403a5ebf4f3854d8
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-09-14 11:15:43 -0300

    feat: implementa página de SignIn

---
+++ Explorer/Stage-09/rocketnotes/src/pages/SignIn/style.js
@@ -0,0 +1,49 @@
+import { styled } from 'styled-components';
+import backgroundImg from '../../assets/sign-in-log-in-bg.png';
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  text-align: center;
+
+  padding: 0 13.6rem;
+
+  > h1 {
+    font-size: 4.8rem;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
+
+  > p {
+    font-size: 1.4rem;
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+  }
+
+  > h2 {
+    font-size: 2.4rem;
+    font-weight: 500;
+
+    margin: 4.8rem 0;
+  }
+
+  > a {
+    margin-top: 12.4rem;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
+`;
+
+export const BackgroundImg = styled.div`
+  flex: 1;
+
+  background: url(${backgroundImg}) no-repeat center center;
+  background-size: cover;
+`;
