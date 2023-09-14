d18cf6ceabc730c5d4ebf8ee028f6655178cfd76
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-09-14 16:56:02 -0300

    chore: Deleta projeto para recomeçar

--- Explorer/Stage-08/RocketMovies/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const usersRouter = require("./usersRoutes/users.routes");
-
-const router = express.Router();
-
-router.use("/users", usersRouter);
-
-module.exports = router;
