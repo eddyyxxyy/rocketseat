93c76a75064029912da7228ddfceea1d8a7e45b6
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-09-15 14:23:57 -0300

    feat: adiciona e testa middleware de tratamento de erros

--- Explorer/Stage-08/rocket-movies/src/index.js
+++ Explorer/Stage-08/rocket-movies/src/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const handleErrors = require('./utils/handleErrors');
+const AppError = require('./utils/AppError');
 
 const app = express();
 const PORT = 8080;
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.use(express.json());
+
+app.get('/', async (req, res, next) => {
+  try {
+    throw new AppError('Testing error handling middleware');
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use(handleErrors);
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
---
+++ Explorer/Stage-08/rocket-movies/src/utils/AppError.js
@@ -0,0 +1,10 @@
+class AppError extends Error {
+  constructor(message, statusCode, isOperational = true) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = isOperational;
+  }
+}
+
+module.exports = AppError;
