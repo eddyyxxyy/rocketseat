40473f1d936cef8d120433bdca74b684afcfd8c9
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-09-15 18:14:36 -0300

    refactor: remove log de erros

--- Explorer/Stage-08/rocket-movies/src/utils/handleErrors.js
+++ Explorer/Stage-08/rocket-movies/src/utils/handleErrors.js
@@ -2,8 +2,6 @@ function handleErrors(err, req, res, next) {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  console.error(err);
-
   res.status(err.statusCode).json({
     status: err.statusCode,
     message: err.message,
