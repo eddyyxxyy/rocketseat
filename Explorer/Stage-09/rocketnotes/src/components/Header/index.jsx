02dc4432b24cf8efa04f08014b600e58642cc0e6
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-09-21 11:02:27 -0300

    feat: adiciona função de signOut

--- Explorer/Stage-09/rocketnotes/src/components/Header/index.jsx
+++ Explorer/Stage-09/rocketnotes/src/components/Header/index.jsx
@@ -1,7 +1,10 @@
 import { Container, Profile, Logout } from './style';
 import { RiShutDownLine } from 'react-icons/ri';
+import { useAuth } from '../../hooks/auth';
 
 export function Header() {
+  const { signOut } = useAuth();
+
   return (
     <Container>
       <Profile to="/profile">
@@ -11,7 +14,7 @@ export function Header() {
           <strong>Edson Pimenta</strong>
         </div>
       </Profile>
-      <Logout>
+      <Logout onClick={signOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
--- Explorer/Stage-09/rocketnotes/src/hooks/auth.jsx
+++ Explorer/Stage-09/rocketnotes/src/hooks/auth.jsx
@@ -14,7 +14,7 @@ export function AuthProvider({ children }) {
       localStorage.setItem('@rocketnotes:user', JSON.stringify(user));
       localStorage.setItem('@rocketnotes:token', token);
 
-      api.defaults.headers.authorization = `Bearer ${token}`;
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
       setData({ user, token });
     } catch (error) {
@@ -26,18 +26,25 @@ export function AuthProvider({ children }) {
     }
   }
 
+  function signOut() {
+    localStorage.removeItem('@rocketnotes:token');
+    localStorage.removeItem('@rocketnotes:user');
+
+    setData({});
+  }
+
   useEffect(() => {
     const token = localStorage.getItem('@rocketnotes:token');
     const user = localStorage.getItem('@rocketnotes:user');
 
     if (token && user) {
-      api.defaults.headers.authorization = `Bearer ${token}`;
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
       setData({ user: JSON.parse(user), token });
     }
   }, []);
 
   return (
-    <AuthContext.Provider value={{ signIn, user: data.user }}>
+    <AuthContext.Provider value={{ signIn, signOut, user: data.user }}>
       {children}
     </AuthContext.Provider>
   );
