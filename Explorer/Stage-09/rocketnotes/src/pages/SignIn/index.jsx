57ef4a3818d0fa038463a7c2e8f0216356e20ae1
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-09-21 10:49:13 -0300

    feat: implementa SignIn por meio de autenticação JWT

--- Explorer/Stage-09/rocketnotes/src/pages/SignIn/index.jsx
+++ Explorer/Stage-09/rocketnotes/src/pages/SignIn/index.jsx
@@ -3,17 +3,37 @@ import { Link } from 'react-router-dom';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 import { FiMail, FiLock } from 'react-icons/fi';
+import { useAuth } from '../../hooks/auth';
+import { useState } from 'react';
 
 export function SignIn() {
+  const { signIn } = useAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSignIn() {
+    signIn({ email, password });
+  }
+
   return (
     <Container>
       <Form>
         <h1>Rocket Notes</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis.</p>
         <h2>Faça seu login</h2>
-        <Input type="email" icon={FiMail} placeholder="E-mail" />
-        <Input type="password" icon={FiLock} placeholder="Senha" />
-        <Button text="Entrar" />
+        <Input
+          type="email"
+          icon={FiMail}
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          icon={FiLock}
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button text="Entrar" onClick={handleSignIn} />
         <Link to="/register">Criar conta</Link>
       </Form>
       <BackgroundImg />
