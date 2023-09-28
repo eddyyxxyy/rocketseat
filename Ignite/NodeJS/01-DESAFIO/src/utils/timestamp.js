9cfc9ddbf2d6ff19703a86c373f5f75e8c8b689b
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-09-28 09:35:33 -0300

    feat: adds hours, seconds and milliseconds to the timestamp

--- Ignite/NodeJS/01-DESAFIO/src/utils/timestamp.js
+++ Ignite/NodeJS/01-DESAFIO/src/utils/timestamp.js
@@ -1,8 +1,12 @@
 export function timestamp() {
   const date = new Date();
   const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based in JavaScript
+  const month = String(date.getMonth() + 1).padStart(2, '0');
   const year = date.getFullYear();
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  const milliseconds = String(date.getMilliseconds()).padStart(3, '0');
 
-  return `${day}/${month}/${year}`;
+  return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}.${milliseconds}`;
 }
