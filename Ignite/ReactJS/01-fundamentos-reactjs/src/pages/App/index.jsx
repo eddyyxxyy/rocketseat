60346450fe3a0a03042eebd88328795ee09ed453
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-09-28 20:53:03 -0300

    feat: starts creating App view (already responsive)

--- Ignite/ReactJS/01-fundamentos-reactjs/src/pages/App/index.jsx
+++ Ignite/ReactJS/01-fundamentos-reactjs/src/pages/App/index.jsx
@@ -1,10 +1,35 @@
 import { Header } from '../../components/Header';
+import { ProfileSideMenu } from '../../components/ProfileSideMenu';
+import { Post } from '../../components/Post';
+
+import leslieAlexanderProfilePicture from '../../assets/leslie-alexander.png';
+import janeCooperProfilePicture from '../../assets/jane-cooper.jpg';
+
+import janeCooperPostContent from '../../postsDataMock/janeCooper.json';
+
 import styles from './styles.module.css';
 
 export function App() {
   return (
     <div className={styles.app}>
       <Header />
+      <div className={styles.content}>
+        <ProfileSideMenu
+          avatarUrl={leslieAlexanderProfilePicture}
+          userName={'Leslie Alexander'}
+          userWork={'UI Designer'}
+        />
+        <main className={styles.posts}>
+          <Post
+            profile={{
+              avatarUrl: janeCooperProfilePicture,
+              userName: 'Jane Cooper',
+              userWork: 'Dev Front End',
+            }}
+            content={janeCooperPostContent}
+          />
+        </main>
+      </div>
     </div>
   );
 }
