cd6047cd683b2e2015a98e47e7142256663ca4cb
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-09-29 14:40:11 -0300

    feat: implementa Header e Sidebar

---
+++ Ignite/ReactJS/01-fundamentos-reactjs-solucao/src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+import { PencilLine } from 'phosphor-react';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1496611246382-3fbcd9b0ae7b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+      />
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/eddyyxxyy.png" />
+        <strong>Edson Pimenta</strong>
+        <span>Web Developer</span>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
