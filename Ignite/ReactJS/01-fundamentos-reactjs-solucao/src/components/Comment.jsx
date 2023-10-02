33951f4059894687dd925a0d807ec6b6699842d5
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-10-02 13:23:09 -0300

    feat: componentiza Avatar

--- Ignite/ReactJS/01-fundamentos-reactjs-solucao/src/components/Comment.jsx
+++ Ignite/ReactJS/01-fundamentos-reactjs-solucao/src/components/Comment.jsx
@@ -1,3 +1,5 @@
+import { Avatar } from './Avatar';
+
 import { ThumbsUp, Trash } from 'phosphor-react';
 
 import styles from './Comment.module.css';
@@ -5,7 +7,7 @@ import styles from './Comment.module.css';
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/eddyyxxyy.png" />
+      <Avatar src="https://github.com/eddyyxxyy.png" hasBorder={false} />
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
--- Ignite/ReactJS/01-fundamentos-reactjs-solucao/src/components/Sidebar.jsx
+++ Ignite/ReactJS/01-fundamentos-reactjs-solucao/src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { Avatar } from './Avatar';
+
 import { PencilLine } from 'phosphor-react';
 
 import styles from './Sidebar.module.css';
@@ -10,7 +12,7 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1496611246382-3fbcd9b0ae7b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/eddyyxxyy.png" />
+        <Avatar src="https://github.com/eddyyxxyy.png" />
         <strong>Edson Pimenta</strong>
         <span>Web Developer</span>
       </div>
