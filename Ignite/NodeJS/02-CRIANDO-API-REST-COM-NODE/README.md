fd1ff08b7e8590aa4bb6b7034d4eea918f2f84d4
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-10-09 16:36:41 -0300

    feat: adds create, list, get one and summarized info methods

--- Ignite/NodeJS/02-CRIANDO-API-REST-COM-NODE/README.md
+++ Ignite/NodeJS/02-CRIANDO-API-REST-COM-NODE/README.md
@@ -2,13 +2,13 @@
 
 ## Functional Requirements
 
-- [ ] The user must be able to create a new transaction;
-- [ ] The user must be able to view a single transaction.
-- [ ] The user must be able to obtain a summary of their own account;
-- [ ] The user must be able to list all the transactions they have already made;
+- [x] The user must be able to create a new transaction;
+- [x] The user must be able to view a single transaction.
+- [x] The user must be able to obtain a summary of their own account;
+- [x] The user must be able to list all the transactions they have already made;
 
 ## Business rules
 
 - [ ] The user can only view transactions made by themselves.
 - [ ] It must be possible to identify the user between requests;
-- [ ] Transactions can be of two types, credit and debit. Credit adds balance to the user's account, debit subtracts balance from the user's account;
+- [x] Transactions can be of two types, credit and debit. Credit adds balance to the user's account, debit subtracts balance from the user's account;
--- Ignite/NodeJS/02-CRIANDO-API-REST-COM-NODE/src/routes/transactionsRoutes.ts
+++ Ignite/NodeJS/02-CRIANDO-API-REST-COM-NODE/src/routes/transactionsRoutes.ts
@@ -1,5 +1,52 @@
+import { randomUUID } from 'node:crypto';
+
 import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+
+import { knex } from '../database';
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.post('/', async (req) => {});
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select();
+
+    return { transactions };
+  });
+
+  app.get('/:id', async (req) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = getTransactionParamsSchema.parse(req.params);
+
+    const transaction = await knex('transactions').where({ id }).first();
+
+    return { transaction };
+  });
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first();
+
+    return { summary };
+  });
+
+  app.post('/', async (req, rep) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    });
+
+    const { title, amount, type } = createTransactionBodySchema.parse(req.body);
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    });
+
+    return rep.status(201).send();
+  });
 }
