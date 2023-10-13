3997600465d14a9c2e793182541f86b1b870efac
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-10-13 18:42:38 -0300

    chore: production configs

--- Ignite/NodeJS/02-CRIANDO-API-REST-COM-NODE/src/env/index.ts
+++ Ignite/NodeJS/02-CRIANDO-API-REST-COM-NODE/src/env/index.ts
@@ -13,8 +13,9 @@ const envSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'test', 'production'])
     .default('development'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
-  PORT: z.number(),
+  PORT: z.coerce.number(),
 });
 
 const _env = envSchema.safeParse({ ...process.env, PORT: port });
